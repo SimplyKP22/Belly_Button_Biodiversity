dd9cdbb45bfd91285a7b4fc143b46967a63aac75
Kadien <kpeart678@gmail.com>
2022-06-26 13:50:55 -0400

    updating the app.js file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,8 +1,11 @@
 // Creating a function to select the dropdown menu and gather the 
-// data from the sample.json file prior to 
+// data from the sample.json file
 function init() {
+
+    // Select the dropdown 
     var selector = d3.select("#selDataset");
-  
+
+    // Build the list that should appear in the dropdown
     d3.json("samples.json").then((data) => {
       console.log(data);
       var sampleNames = data.names;
@@ -11,33 +14,57 @@ function init() {
           .append("option")
           .text(sample)
           .property("value", sample);
+      
       });
-  })}
-  
-  init();
-
-  // optionChanged function is called in the index.html
-  function optionChanged(Sample) {
-    console.log(Sample);
-    Metadata(Sample);
-    buildCharts(Sample);
-  }
-
-  // Metadata function called in optionChanged
-  function Metadata(sample) {
-    d3.json("samples.json").then((data) => {
+      
       var metadata = data.metadata;
       var resultsArray = metadata.filter(sampleObj => sampleObj.id == sample);
       var result = resultsArray[0]; 
-      var shortDisplay = d3.select("#sample-metadata");
+      var demographicInfo = d3.select("#sample-metadata");
 
-      shortDisplay.html(""); 
-      shortDisplay.append("h6").text(`ID: ${result.id}`);
-      shortDisplay.append("h6").text(`Ethnicity: ${result.ethnicity}`);
-      shortDisplay.append("h6").text(`Gender: ${result.gender}`);
-      shortDisplay.append("h6").text(`Age: ${result.age}`);
-      shortDisplay.append("h6").text(`Location: ${result.location}`);
-      shortDisplay.append("h6").text(`BBtype: ${result.bbtype}`);
-      shortDisplay.append("h6").text(`Washing/week: ${result.wfreq}`);
+      demographicInfo.html(""); 
+      demographicInfo.append("h6").text(`ID: ${result.id}`);
+      demographicInfo.append("h6").text(`Ethnicity: ${result.ethnicity}`);
+      demographicInfo.append("h6").text(`Gender: ${result.gender}`);
+      demographicInfo.append("h6").text(`Age: ${result.age}`);
+      demographicInfo.append("h6").text(`Location: ${result.location}`);
+      demographicInfo.append("h6").text(`BBtype: ${result.bbtype}`);
+      demographicInfo.append("h6").text(`Washing/week: ${result.wfreq}`);
+    })
+
+
+}
+
+  init();
+  
+
+  // optionChanged function is called in the index.html
+  function optionChanged(Sample) {
+    // Print the Test Subject ID No. that prompted the change to the console
+    console.log(Sample);
+    // Call the Metadata function to change the display of the new demographic
+    // data for the selected ID No. 
+    Metadata(Sample);
+    // Call the new Charts function to update the charts the relavant data for the
+    // selected ID No. 
+    newCharts(Sample);
+  }
+
+  // Metadata function called in optionChanged
+  function Metadata(Sample) {
+    d3.json("samples.json").then((data) => {
+      var metadata = data.metadata;
+      var resultsArray = metadata.filter(sampleObj => sampleObj.id == Sample);
+      var result = resultsArray[0]; 
+      var demographicInfo = d3.select("#sample-metadata");
+
+      demographicInfo.html(""); 
+      demographicInfo.append("h6").text(`ID: ${result.id}`);
+      demographicInfo.append("h6").text(`Ethnicity: ${result.ethnicity}`);
+      demographicInfo.append("h6").text(`Gender: ${result.gender}`);
+      demographicInfo.append("h6").text(`Age: ${result.age}`);
+      demographicInfo.append("h6").text(`Location: ${result.location}`);
+      demographicInfo.append("h6").text(`BBtype: ${result.bbtype}`);
+      demographicInfo.append("h6").text(`Washing/week: ${result.wfreq}`);
     });
   }
